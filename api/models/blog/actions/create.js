4c612f28ab48758b7f22bcc1d84f32dc5a934bdd
Antoine Charette <antoineocharette@gmail.com>
2024-03-20 16:46:33 -0400

    Added live to the query in the frontend // Removed phone number

--- api/models/blog/actions/create.js
+++ api/models/blog/actions/create.js
@@ -25,7 +25,7 @@ export async function onSuccess({ params, record, logger, api, connections }) {
   client.messages
     .create({
       from: process.env.TWILIO_PHONE_NUMBER,
-      to: "6138893035", // INSERT NUMBER HERE
+      to: "", // INSERT NUMBER HERE
       body: `Your newly created blog, ${record.title}, is ready to publish!\n\nWould you like to publish the blog at this time? (Yes/No)`,
     })
     .then((response) => {
--- web/routes/index.jsx
+++ web/routes/index.jsx
@@ -15,6 +15,7 @@ export default function () {
         published: true,
         authorName: true,
       },
+      live: true,
     });
 
   if (fetchingBlog) {
